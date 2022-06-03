de3c31afcd7cff971ed16068cd87d2eca44066a8
RoelKneepkens <roelkneepkens@shipitsmarter.com>
2022-06-03 11:23:23 +0200

    feat: use a loop for package types

--- src/extension.ts
+++ src/extension.ts
@@ -3,25 +3,36 @@ import { LabelaryPanel } from './panels/labelary_panel';
 
 
 export function activate(context: vscode.ExtensionContext) {
-	// Command for default label
-	const label = vscode.commands.registerCommand('labelary.view-label', () => {
-		LabelaryPanel.render(context.extensionUri, context);
-	});
+	let labelsizes = ["8.25x11.75", "4x8", "8x4"];
+
+	labelsizes.forEach((value, index) => {
+		const commandbutton: string = `labelary.view-label${value}`;
+		const label = vscode.commands.registerCommand(commandbutton, () => {
+			LabelaryPanel.render(context.extensionUri, context, value);
+		});
+		
+		context.subscriptions.push(label);
+	  });
+
+	// // Command for default label
+	// const label = vscode.commands.registerCommand('labelary.view-label', () => {
+	// 	LabelaryPanel.render(context.extensionUri, context);
+	// });
 	
-	context.subscriptions.push(label);
+	// context.subscriptions.push(label);
 
 
-	// Command for raw ZPL label
-	const labelA4 = vscode.commands.registerCommand('labelary.view-labelA4', () => {
-		LabelaryPanel.render(context.extensionUri, context, "8.25x11.75");
-	});
+	// // Command for raw ZPL label
+	// const labelA4 = vscode.commands.registerCommand('labelary.view-labelA4', () => {
+	// 	LabelaryPanel.render(context.extensionUri, context, "8.25x11.75");
+	// });
 	
-	context.subscriptions.push(labelA4);	
+	// context.subscriptions.push(labelA4);	
 
-	// Command for raw ZPL label
-	const label4x8 = vscode.commands.registerCommand('labelary.view-label4x8', () => {
-		LabelaryPanel.render(context.extensionUri, context, "4x8");
-	});
+	// // Command for raw ZPL label
+	// const label4x8 = vscode.commands.registerCommand('labelary.view-label4x8', () => {
+	// 	LabelaryPanel.render(context.extensionUri, context, "4x8");
+	// });
 	
-	context.subscriptions.push(label4x8);
+	// context.subscriptions.push(label4x8);
 }
