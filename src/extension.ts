93743b594f81410272eba7d45f3073d75f714a93
bvanderhorn <bvanderhorn@gmail.com>
2022-06-03 13:04:07 +0200

    feat: add custom submenu item

--- src/extension.ts
+++ src/extension.ts
@@ -5,34 +5,35 @@ import { LabelaryPanel } from './panels/labelary_panel';
 export function activate(context: vscode.ExtensionContext) {
 	let labelsizes = ["8.25x11.75", "4x8", "8x4"];
 
-	labelsizes.forEach((value, index) => {
-		const commandbutton: string = `labelary.view-label${value}`;
-		const label = vscode.commands.registerCommand(commandbutton, () => {
-			LabelaryPanel.render(context.extensionUri, context, value);
-		});
-		
-		context.subscriptions.push(label);
-	  });
-
-	// // Command for default label
-	// const label = vscode.commands.registerCommand('labelary.view-label', () => {
-	// 	LabelaryPanel.render(context.extensionUri, context);
-	// });
-	
-	// context.subscriptions.push(label);
+	// Command for default label
+	const label = vscode.commands.registerCommand('labelary.view-labeldefault', () => {
+		LabelaryPanel.render(context.extensionUri, context);
+	});	
+	context.subscriptions.push(label);
 
 
-	// // Command for raw ZPL label
-	// const labelA4 = vscode.commands.registerCommand('labelary.view-labelA4', () => {
-	// 	LabelaryPanel.render(context.extensionUri, context, "8.25x11.75");
-	// });
-	
-	// context.subscriptions.push(labelA4);	
+	// Command for A4 label
+	const labelA4 = vscode.commands.registerCommand('labelary.view-labelA4', () => {
+		LabelaryPanel.render(context.extensionUri, context, "8.25x11.75");
+	});	
+	context.subscriptions.push(labelA4);	
 
-	// // Command for raw ZPL label
-	// const label4x8 = vscode.commands.registerCommand('labelary.view-label4x8', () => {
-	// 	LabelaryPanel.render(context.extensionUri, context, "4x8");
-	// });
-	
-	// context.subscriptions.push(label4x8);
+	// Command for 4x8 label
+	const label4x8 = vscode.commands.registerCommand('labelary.view-label4x8', () => {
+		LabelaryPanel.render(context.extensionUri, context, "4x8");
+	});	
+	context.subscriptions.push(label4x8);
+
+	// Command for 8x4 label
+	const label8x4 = vscode.commands.registerCommand('labelary.view-label8x4', () => {
+		LabelaryPanel.render(context.extensionUri, context, "8x4");
+	});	
+	context.subscriptions.push(label8x4);
+
+
+	// Command for default label
+	const labelcustom = vscode.commands.registerCommand('labelary.view-labelcustom', () => {
+		LabelaryPanel.render(context.extensionUri, context);
+	});	
+	context.subscriptions.push(labelcustom);
 }
